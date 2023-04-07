b4a9fc1731ad44d4fc73cc4b41d6e2866ca5bd08
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-07 20:13:15 +0300

    feat: backend apiserver layout created

---
+++ byte-battle_backend/internal/app/apiserver/apiserver.go
@@ -0,0 +1,54 @@
+package apiserver
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIserver struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+func New(config *Config) *APIserver {
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIserver) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+	s.logger.Info("Starting API server...")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+func (s *APIserver) configureRouter() {
+	s.router.HandleFunc("/api", s.handleApiList())
+}
+
+func (s *APIserver) handleApiList() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "API root")
+	}
+}
