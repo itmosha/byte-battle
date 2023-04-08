d8aaf98526ace9a4dc02098b637d9630daec9e16
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-08 13:49:08 +0300

    feat: server config separated from main

--- byte-battle_backend/cmd/apiserver/main.go
+++ byte-battle_backend/cmd/apiserver/main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"flag"
 	"log"
 
 	"github.com/Mihalych32/byte-battle/tree/main/byte-battle_backend/internal/app/apiserver"
 )
 
-var (
-	configPath string
-)
-
-func init() {
-	flag.StringVar(&configPath, "config-path", "configs/apiserver.yaml", "path to the apiserver config file")
-}
-
 func main() {
-	flag.Parse()
-
 	config := apiserver.NewConfig()
 
 	s := apiserver.New(config)
