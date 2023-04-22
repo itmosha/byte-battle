c2f3d047ba5f2f9f319110ba3c417d6d796004cd
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-22 16:45:29 +0300

    feat: Implement login

--- byte-battle_frontend/src/components/LoginWindow.tsx
+++ byte-battle_frontend/src/components/LoginWindow.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 
 interface Props {
@@ -7,6 +6,49 @@ interface Props {
 }
 
 function LoginWindow(props: Props): JSX.Element {
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+
+        switch (id) {
+            case "username" : {
+                setUsername(value);
+                break;
+            }
+            case "password" : {
+                setPassword(value);
+                break;
+            }
+        }
+    }
+
+    const handleSubmit = async () => {
+        try {
+            const url = `${import.meta.env.PUBLIC_PROTOCOL}://${import.meta.env.PUBLIC_HOSTNAME}:8080/api/login/`;
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify({
+                    "Username": username,
+                    "Password": password,
+                })
+            });
+
+            if (response.status === 200) {
+                const responseJSON = await response.json();
+
+                localStorage.setItem("token", responseJSON.token);
+                props.closeWindow(false);
+            } else {
+                alert('Failed!');
+            }
+        } catch (err: any) {
+            console.log(err);
+        }
+    }
+
     return (
         <div
             className="w-screen h-screen absolute z-10 
@@ -20,12 +62,26 @@ function LoginWindow(props: Props): JSX.Element {
                     <h1 className="font-custom text-3xl">Login</h1>
                 </div>
                 <div className="items-center">
-                    <input type="text" placeholder="Username" id="username" className="w-[256px] my-1 p-2 rounded-md bg-gray-900/50 border-2 border-gray-900/40 focus:border-blue-800 outline-none"></input>
-                    <input type="password" placeholder="Password" id="password" className="w-[256px] my-1 p-2 rounded-md bg-gray-900/50 border-2 border-gray-900/40 focus:border-blue-800 outline-none"></input>
+                    <input 
+                        type="text" 
+                        placeholder="Username" 
+                        id="username" 
+                        value={username} 
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+                        className="w-[256px] my-1 p-2 rounded-md bg-gray-900/50 border-2 border-gray-900/40 focus:border-blue-800 outline-none"
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        id="password" 
+                        value={password} 
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+                        className="w-[256px] my-1 p-2 rounded-md bg-gray-900/50 border-2 border-gray-900/40 focus:border-blue-800 outline-none"
+                    />
                     <div className="flex px-12 mt-8">
                         <button 
                             type="submit" 
-                            onClick={() => props.closeWindow(false)}
+                            onClick={() => { handleSubmit(); } }
                             className="bg-blue-900 w-[160px] rounded-md py-2 hover:bg-blue-800 hover:shadow-blue-800/10 shadow-lg transition ease-in-out duration-200 font-custom"
                         >
                             Sign in
